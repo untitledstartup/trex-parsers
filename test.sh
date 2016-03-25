8bc6041280c9327b429dfa1566d5851e6c365b5f
Pasha <pasha@2vega.com>
2016-03-25 15:28:33 -0700

    fix up tester

--- test.sh
+++ test.sh
@@ -1,9 +1,15 @@
 #!/bin/zsh
 
+autoload -U colors && colors
+
 echo "Testing Obj-C"
 
-for i in samples/objc/*.m; do
-  found=$(./lib/genstrings.js $i | grep "label" | wc -l); 
-  exist=$(grep "^ *TMLLocalizedString" $i | wc -l); 
-  echo "$i $found $exist"; 
+for i in samples/**/*.*(.); do
+  found=$(./lib/genstrings.js $i | sed -e 's/,/\'$'\n/g' | grep -E "^\[?{\"label\":" | wc -l | sed "s/[^0-9]//g" );
+  exist=$(grep -E "\\bTMLLocalizedString *\(" $i | wc -l | sed "s/[^0-9]//g" ); 
+  if [[ $found -eq $exist ]]; then
+    echo "$fg[green]$i $found $exist $reset_color";
+  else
+    echo "$fg[red]$i $found $exist $reset_color";
+  fi
 done
