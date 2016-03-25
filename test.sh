c88b3b444bbd5368fcea2be6006cd1cfbb3a70e1
Pasha <pasha@2vega.com>
2016-03-25 15:45:05 -0700

    prepare for multiple langs

--- test.sh
+++ test.sh
@@ -2,14 +2,16 @@
 
 autoload -U colors && colors
 
-echo "Testing Obj-C"
-
-for i in samples/**/*.*(.); do
-  found=$(./lib/genstrings.js -p $i | grep -E "^ *\"label\":" | wc -l | sed "s/[^0-9]//g" );
-  exist=$(grep -E "\\bTMLLocalizedString *\(" $i | wc -l | sed "s/[^0-9]//g" ); 
-  if [[ $found -eq $exist ]]; then
-    echo "$fg[green]$i $found $exist $reset_color";
-  else
-    echo "$fg[red]$i $found $exist $reset_color";
-  fi
+for l in samples/*; do
+  echo "Testing ${l}"
+  for i in samples/${l:t}/*.*(.); do
+    found=$(./lib/genstrings.js -p $i | grep -E "^ *\"label\":" | wc -l | sed "s/[^0-9]//g" );
+    exist=$(grep -E "\\bTMLLocalizedString *\(" $i | wc -l | sed "s/[^0-9]//g" ); 
+    if [[ $found -eq $exist ]]; then
+      echo "$fg[green]$i $found $exist $reset_color";
+    else
+      echo "$fg[red]$i $found $exist $reset_color";
+    fi
+  done
+  echo
 done
