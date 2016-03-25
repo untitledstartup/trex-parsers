8fc1ee9cf70b7ff3fe38c122db5a11cf54c7365b
Pasha <pasha@2vega.com>
2016-03-25 15:36:18 -0700

    more reliable test

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ autoload -U colors && colors
 echo "Testing Obj-C"
 
 for i in samples/**/*.*(.); do
-  found=$(./lib/genstrings.js $i | sed -e 's/,/\'$'\n/g' | grep -E "^\[?{\"label\":" | wc -l | sed "s/[^0-9]//g" );
+  found=$(./lib/genstrings.js -p $i | grep -E "^ *\"label\":" | wc -l | sed "s/[^0-9]//g" );
   exist=$(grep -E "\\bTMLLocalizedString *\(" $i | wc -l | sed "s/[^0-9]//g" ); 
   if [[ $found -eq $exist ]]; then
     echo "$fg[green]$i $found $exist $reset_color";
