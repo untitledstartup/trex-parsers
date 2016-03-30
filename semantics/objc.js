185aaed4951756b5afa7af5a1275c4cab90369ef
Pasha <pasha@2vega.com>
2016-03-30 10:33:45 -0700

    factor out translation key creation into utils module
    update all semantics to utilize new key creation method

--- semantics/objc.js
+++ semantics/objc.js
@@ -1,87 +1,87 @@
-module.exports = {
-  "attributes": {
-    "translationKeys": {
-      "TMLLocalizedString": function(macro, open, label, sep, args, close, semicolon) {
-        var results = [];
-        var labelString = label.translationKeys;
-        if (labelString.length > 0) {
-          results.push(labelString);
-        }
-        else {
-          return null;
-        }
-        var argResult = args.translationKeys;
-        if (argResult && argResult.length > 0) {
-          var argStr = argResult[0];
-          if (argStr instanceof Array) {
-            argStr = argStr[0];
+(function(){
+  var utils = require('../lib/GenstringUtils');
+  
+  module.exports = {
+    "attributes": {
+      "translationKeys": {
+        "TMLLocalizedString": function(macro, open, label, sep, args, close, semicolon) {
+          var results = [];
+          var labelString = label.translationKeys;
+          if (labelString.length > 0) {
+            results.push(labelString);
           }
-          if (argStr) {
-            results.push(argStr);
+          else {
+            return null;
           }
-        }
-        var info = {"label": results[0]};
-        if (results.length > 1) {
-          info["description"] = results[1];
-        }
-        debugger;
-        return info;
-      },
-      "Macro": function(e) {
-        return "";
-      },
-      "Property": function(first, sep, seconds) {
-        return "";
-      },
-      "Object": function(e) {
-        return e.translationKeys;
-      },
-      "NumberObject": function(_, num) {
-        return "";
-      },
-      "StringObject": function(_, str, _, additionalLines) {
-        var result = str.translationKeys;
-        if (additionalLines && additionalLines.interval.contents.length > 0) {
-          var additionalString = additionalLines.translationKeys;
-          if (additionalString) {
-            result += "\n" + additionalString;
+          var argResult = args.translationKeys;
+          if (argResult && argResult.length > 0) {
+            var argStr = argResult[0];
+            if (argStr instanceof Array) {
+              argStr = argStr[0];
+            }
+            if (argStr) {
+              results.push(argStr);
+            }
           }
+          var key = utils.createTranslationKey(results[0], results[1]);
+          return key;
+        },
+        "Macro": function(e) {
+          return "";
+        },
+        "Property": function(first, sep, seconds) {
+          return "";
+        },
+        "Object": function(e) {
+          return e.translationKeys;
+        },
+        "NumberObject": function(_, num) {
+          return "";
+        },
+        "StringObject": function(_, str, _, additionalLines) {
+          var result = str.translationKeys;
+          if (additionalLines && additionalLines.interval.contents.length > 0) {
+            var additionalString = additionalLines.translationKeys;
+            if (additionalString) {
+              result += "\n" + additionalString;
+            }
+          }
+          return result;
+        },
+        "Dict": function(open, parts, close) {
+          return "";
+        },
+        "DictEntry": function(key, _, val) {
+          return "";
+        },
+        "DictValue": function(e) {
+          return e.translationKeys;
+        },
+        "Array": function(open, parts, close) {
+          return "";
+        },
+        "ArrayEntry": function(e) {
+          return e.translationKeys;
+        },
+        "CollectionAccess": function(varname, open, arg, close) {
+          return "";
+        },
+        "MessageSend": function(msg) {
+          return "";
+        },
+        "NoArgumentMessageSend": function(_, receiver, messageName, _) {
+          return "";
+        },
+        "ArgumentMessageSend": function(_, receiver, messageNames, args, _) {
+          return args.translationKeys;
+        },
+        "MessageComponent": function(parts) {
+          return "";
+        },
+        "messageComponentChar": function(e) {
+          return "";
         }
-        return result;
-      },
-      "Dict": function(open, parts, close) {
-        return "";
-      },
-      "DictEntry": function(key, _, val) {
-        return "";
-      },
-      "DictValue": function(e) {
-        return e.translationKeys;
-      },
-      "Array": function(open, parts, close) {
-        return "";
-      },
-      "ArrayEntry": function(e) {
-        return e.translationKeys;
-      },
-      "CollectionAccess": function(varname, open, arg, close) {
-        return "";
-      },
-      "MessageSend": function(msg) {
-        return "";
-      },
-      "NoArgumentMessageSend": function(_, receiver, messageName, _) {
-        return "";
-      },
-      "ArgumentMessageSend": function(_, receiver, messageNames, args, _) {
-        return args.translationKeys;
-      },
-      "MessageComponent": function(parts) {
-        return "";
-      },
-      "messageComponentChar": function(e) {
-        return "";
       }
     }
   }
-}
+})()
--- semantics/swift.js
+++ semantics/swift.js
@@ -1,63 +1,62 @@
-module.exports = {
-  "attributes": {
-    "translationKeys": {
-      "TMLLocalizedString": function(macro, open, label, sep, args, close) {
-        var results = [];
-        var labelString = label.translationKeys;
-        if (labelString.length > 0) {
-          results.push(labelString);
-        }
-        else {
-          return null;
-        }
-        var argResult = args.translationKeys;
-        if (argResult && argResult.length > 0) {
-          var argStr = argResult[0];
-          if (argStr instanceof Array) {
-            argStr = argStr[0];
+(function(){
+  var utils = require('../lib/GenstringUtils');
+  module.exports = {
+    "attributes": {
+      "translationKeys": {
+        "TMLLocalizedString": function(macro, open, label, sep, args, close) {
+          var results = [];
+          var labelString = label.translationKeys;
+          if (labelString.length > 0) {
+            results.push(labelString);
           }
-          if (argStr) {
-            results.push(argStr);
+          else {
+            return null;
           }
+          var argResult = args.translationKeys;
+          if (argResult && argResult.length > 0) {
+            var argStr = argResult[0];
+            if (argStr instanceof Array) {
+              argStr = argStr[0];
+            }
+            if (argStr) {
+              results.push(argStr);
+            }
+          }
+          var key = utils.createTranslationKey(results[0], results[1]);
+          return key;
+        },
+        "StringLiteral": function(strings) {
+          var result = strings.translationKeys;
+          if (result instanceof Array) {
+            result = result.join("\n");
+          }
+          return result;
+        },
+        "Macro": function(e) {
+          return "";
+        },
+        "NamedArg": function(varName, sep, arg) {
+          return arg.translationKeys;
+        },
+        "Dict": function(open, parts, close) {
+          return "";
+        },
+        "DictEntry": function(key, _, val) {
+          return "";
+        },
+        "DictValue": function(e) {
+          return e.translationKeys;
+        },
+        "Array": function(open, parts, close) {
+          return "";
+        },
+        "ArrayEntry": function(e) {
+          return e.translationKeys;
+        },
+        "CollectionAccess": function(varname, open, arg, close) {
+          return "";
         }
-        var info = {"label": results[0]};
-        if (results.length > 1) {
-          info["description"] = results[1];
-        }
-        debugger;
-        return info;
-      },
-      "StringLiteral": function(strings) {
-        var result = strings.translationKeys;
-        if (result instanceof Array) {
-          result = result.join("\n");
-        }
-        return result;
-      },
-      "Macro": function(e) {
-        return "";
-      },
-      "NamedArg": function(varName, sep, arg) {
-        return arg.translationKeys;
-      },
-      "Dict": function(open, parts, close) {
-        return "";
-      },
-      "DictEntry": function(key, _, val) {
-        return "";
-      },
-      "DictValue": function(e) {
-        return e.translationKeys;
-      },
-      "Array": function(open, parts, close) {
-        return "";
-      },
-      "ArrayEntry": function(e) {
-        return e.translationKeys;
-      },
-      "CollectionAccess": function(varname, open, arg, close) {
-        return "";
       }
     }
   }
-}
+})()
