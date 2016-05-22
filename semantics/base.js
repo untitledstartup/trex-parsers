2bc6626df7bc3f935f678c612821e096913f3d31
Pasha <pasha@2vega.com>
2016-05-21 20:53:57 -0700

    Parsing using tokens
    * Using Ohm for defining how we identify tokens - we shouldn't have too many of these, and it's more convenient than having to change parser logic just because character sequences and chars are different, while structures are similar
    * TokenParser is a base class for a token parser, which currently detects strings in a sequence "macro" "(" ( * | "string")* ")" and turns them into translation keys. It uses specific operators to identify subexpressions and ignores string found while inside sub-expression, but doesn't ignore new macros... So it should identify things like: tr("string", {...}, "string") or TMLLocalizedString(@"Outer localized string", @{@"label": TMLLocalizedString(@"Inner localized String")}, @[], nil, "", @"Outer localized comment");

--- semantics/base.js
+++ semantics/base.js
@@ -1,304 +1,59 @@
 (function(){
   
-  var GSUtils = require('../lib/utils.js');
+  var utils = require('../lib/utils.js');
   
   module.exports = {
     "attributes": {
-      "translationKeys": {
-        // Basic support
-        "ListOf_none": function() {
+      "tokens": {
+        
+        "nil": function (n) {
+          return utils.createResult(this, n);
+        },
+        "bool": function (n) {
+          return utils.createResult(this, n);
+        },
+        
+        "number": function (n) {
+          return utils.createResult(this, n);
+        },
+        "hexNumber": function (_minus, prefix, digits) {
           return null;
         },
-        "ListOf_some": function(first, separator, iter) {
-          var result = [];
-          var firstString = first.translationKeys;
-          if (firstString) {
-            result.push(firstString);
-          }
-          var children = iter.children;
-          for (var i = 0; i < children.length; i++) {
-            var child = children[i];
-            var childString = child.translationKeys;
-            if (childString) {
-              result.push(childString);
-            }
-          }
-          result = GSUtils.createResult("ListOf_some", result);
-          // debugger;
-          return result;
-        },
-        "listOf_none": function() {
+        "octalNumber": function (_minus, prefix, digits) {
           return null;
         },
-        "listOf_some": function(first, separator, iter) {
-          var result = [];
-          var firstKey = first.translationKeys;
-          if (firstKey) {
-            result.push(firstKey);
-          }
-          var children = iter.children;
-          for (var i = 0; i < children.length; i++) {
-            var child = children[i];
-            var childKey = child.translationKeys;
-            if (childKey) {
-              result.push(childKey);
-            }
-          }
-          result = GSUtils.createResult("listOf_some", result);
-          // debugger;
-          return result;
-        },
-      
-        // space* listOf<(tmlStatement|nonTMLStatement), statementSep+> (statementSep|space)*
-        "tmlLocalizedStrings": function(_, statements, tail) {
-          var result = statements.translationKeys;
-          if (!result) {
-            return null;
-          }
-          var keys = GSUtils.collectTranslationKeysFromObjects(result);
-          if (!keys) {
-            return null;
-          }
-          result = GSUtils.createResult("tmlLocalizedStrings", keys);
-          // debugger;
-          return result;
-        },
-        // methodNamed<macro>
-        "tmlLocalizedString": function(meth) {
-          var result = meth.translationKeys;
-          // debugger;
-          if (!result) {
-            return null;
-          }
-          result = GSUtils.createResult("tmlLocalizedString", result);
-          result.flatten();
-          // debugger;
-          return result;
-        },
-        // ~tmlLocalizedString (~statementSep any)+
-        "nonTMLStatement": function(chars) {
+        "expNumber": function (decimal, suffix, _minus, expDigits) {
           return null;
         },
-        // tmlLocalizedString
-        "tmlStatement": function(tmlString) {
-          var result = tmlString.translationKeys;
-          if (!result) {
-            return null;
-          }
-          var result = GSUtils.createResult("tmlStatement", result);
-          // debugger;
-          return result;
-        },
-      
-        // Literals
-        "nullLiteral": function(e) {
+        "decimal": function (nodes) {
           return null;
         },
-        "booleanLiteral": function(parts) {
-          return null;
+        
+        "string": function (node) {
+          var result = node.tokens;
+          result = utils.createResult(this, result.value);
+          return result;
         },
-        "variable": function(first, rest) {
-          return null;
-        },
-        "literal": function(e) {
-          var result = e.translationKeys;
-          if (!result) {
-            return null;
-          }
-          result = GSUtils.createResult("literal", result);
+        "quotedString": function (quot, chars, quot) {
           // debugger;
-          return result;
+          return utils.createResult(this, chars);
         },
-        "numberLiteral": function(integers, _, decimals) {
-          return null;
+        
+        "operator": function (op) {
+          return utils.createResult(this, op);
         },
-      
-        // Expressions
-        "macro": function(e) {
-          return null;
+        "openExpressionOperator": function (op) {
+          return utils.createResult(this, op);
         },
-        "property": function(first, _, rest) {
-          return null;
+        "closeExpressionOperator": function (op) {
+          return utils.createResult(this, op);
         },
-        // aMethodName "(" space* listOf<argExp, argSep > space* ")"
-        "methodNamed": function(methodName, _, _, args, _, _) {
-          var meth = methodName.interval.contents;
-          var match = methodName._node.grammar.match(meth, "macro");          
-          if (!match.succeeded()) {
-            return null;
-          }
-          
-          var result = args.translationKeys;
-          // debugger;
-          result.flatten();
-          var keys = result.results;
-          if (keys.length >= 2) {
-            var label = null;
-            var desc = null;
-            for (var i=0; i<keys.length; i++) {
-              var key = keys[i];
-              if (!key) {
-                continue;
-              }
-              if (!label) {
-                label = key.label;
-              }
-              else if (!desc) {
-                desc = key.label;
-              }
-              if (label && desc) {
-                break;
-              }
-            }
-            var newKey = GSUtils.createTranslationKey(label, desc);
-            result.results = [newKey];
-          }
-          result = GSUtils.createResult("methodNamed", result);
-          return result;
-        },
-      
-        // Strings
-        "quotedStringLiteral": function(quoteChar, str, _) {
-          var string = str.interval.contents;
-          if (!string) {
-            return null;
-          }
-          var quoteStr = quoteChar.interval.contents;
-          var re = new RegExp("\\\\" + quoteStr, "g");
-          // debugger;
-          string = string.replace(re, quoteStr);
-          var key = GSUtils.createTranslationKey(string);
-          var result = GSUtils.createResult("quotedStringLiteral", key);
-          // debugger;
-          return result;
-        },
-        "quotedStringChars": function(chars) {
-          var string = chars.interval.contents;
-          var key = GSUtils.createTranslationKey(string);
-          var result = GSUtils.createResult("quotedStringChars", key);
-          // debugger;
-          return result;
-        },
-      
-        // Arguments
-        "arg": function(arg) {
-          var result = arg.translationKeys;
-          if (!result) {
-            return null;
-          }
-          result = GSUtils.createResult("arg", result);
-          // debugger;
-          return result;
-        },
-        // "(" space* argExp space* ")"
-        "argExp_parens": function(_, _, argExp, _, _) {
-          var result = argExp.translationKeys;
-          if (!result) {
-            return null;
-          }
-          result = GSUtils.createResult("argExp_parens", result);
-          // debugger;
-          return result;
-        },
-        // argExp space* ternaryOperator space* argExp space* ternaryOperator space* argExp
-        "argExp_ternary": function(first, _, op, _, second, _, op, _, third) {
-          var result = [];
-          var firstKeys = first.translationKeys;
-          if (firstKeys) {
-            result.push(firstKeys);
-          }
-          var secondKeys = second.translationKeys;
-          if (secondKeys) {
-            result.push(secondKeys);
-          }
-          var thirdKeys = third.translationKeys;
-          if (thirdKeys) {
-            result.push(thirdKeys);
-          }
-          result = GSUtils.createResult("argExp_ternary", result);
-          // debugger;
-          return result;
-        },
-        // argExp space* binaryOperator space* argExp
-        "argExp_binary": function(first, _, op, _, second) {
-          var result = [first.translationKeys, second.translationKeys];
-          if (!result) {
-            return null;
-          }
-          result = GSUtils.createResult("argExp_binary", result);
-          // debugger;
-          return result;
-        },
-        // unaryOperator space* argExp
-        "argExp_unary": function(op, _, arg) {
-          var result = arg.translationKeys;
-          if (!result) {
-            return null;
-          }
-          result = GSUtils.createResult("argExp_unaryAfter", result);
-          // debugger;
-          return result;
-        },
-        // argExp space* unaryOperator 
-        "argExp_unaryAfter": function(arg, _, op) {
-          var result = arg.translationKeys;
-          if (!result) {
-            return null;
-          }
-          result = GSUtils.createResult("argExp_unaryAfter", result);
-          // debugger;
-          return result;
-        },
-      
-        // XML
-        // "<" space* tag space* listOf<xmlTagAttributeExp, space+> space* "/"? space* ">"
-        "xmlTag": function(open, _, tag, _, attrs, _, _, _, close) {
-          var keys = attrs.translationKeys;
-          if (!keys) {
-            return null;
-          }
-          var result = GSUtils.createResult("xmlTag", keys);
-          return result;
-        },
-        // "<" space* tag (space+ ~(attr | ">") xmlTagAttributeExp)* space+ attr (space+ xmlTagAttributeExp)* space* "/"? space* ">"
-        "xmlTagWithAttribute": function(open, _, tag, _, ignoreAttrs, _, attr, _, remainder, _, slash, _, close) {
-          var keys = attr.translationKeys;
-          if (!keys) {
-            return null;
-          }
-          var result = GSUtils.createResult("xmlTagWithAttribute", keys);
-          return result;
-        },
-        // xmlTagAttributeName ("=" xmlTagAttributeValue)?
-        "xmlTagAttributeExp": function(attr, _, value) {
-          var result = null;
-          if (value && value.interval.contents.length > 0) {
-            result = value.translationKeys;
-          }
-          if (!result) {
-            return null;
-          }
-          result = GSUtils.createResult("xmlTagAttributeExp", result);
-          return result;
-        },
-        // XMLTag<tag> content XMLCloseTag<tag>
-        "xmlTagWithContent": function(tag, tagContent, close) {
-          var keys = tagContent.translationKeys;
-          if (!keys) {
-            return null;
-          }
-          var result = GSUtils.createResult("xmlTagWithContent", keys);
-          return result;
-        },
-        // XMLTagWithAttribute<tag, attr> content XMLCloseTag<tag>
-        "xmlTagWithAttributeAndContent": function(tagWithAttr, content, close) {
-          var keys = content.translationKeys;
-          if (!keys) {
-            return null;
-          }
-          var result = GSUtils.createResult("xmlTagWithAttributeAndContent", keys);
+        
+        "namedToken": function (chars) {
+          var result = utils.createResult(this, chars);
           return result;
         }
+      
       }
     }
   }
