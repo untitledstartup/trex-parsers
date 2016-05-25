eff12ce77b6c6407ee4ad0526a73e9c5a42ad126
Pasha <pasha@2vega.com>
2016-05-25 13:10:12 -0700

    parse strings out of TRL expressions

--- semantics/html.js
+++ semantics/html.js
@@ -13,11 +13,39 @@
         "closeTag": function (n) {
           return utils.createResult(this, n);
         },
-        "tagText": function (close, text) {
+        "tagText_normal": function (close, text) {
           var a = close.tokens;
-          var b = utils.createResult(this, text);
+          var b = utils.createResult("tagText", text);
           return [a, b];
-        }
+        },
+        "tagText_trl": function (_, _, exp, _) {
+          return exp.tokens;
+        },
+        
+        
+        
+        "quotedTRLExp": function (quot, exp, quot) {
+          var result = exp.tokens;
+          return result;
+        },
+        "trlExp": function (_, _, exp, _, _) {
+          var result = exp.tokens;
+          if (result) {
+            result = utils.createResult(this, result.value);
+          }
+          return result;
+        },
+        "trlMethod": function (_, _, _, str, _, _) {
+          var result = str.tokens;
+          result = utils.createResult(this, result.value);
+          return result;
+        },
+        "trlFilter": function (str, _, _, _, _) {
+          var result = str.tokens;
+          result = utils.createResult(this, result.value);
+          return result;
+        },
+        
         
       }
     }
