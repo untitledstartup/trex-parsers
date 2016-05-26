6d1452de624e7e552c979ad9c9571b41c22f1106
Pasha <pasha@2vega.com>
2016-05-25 20:53:31 -0700

    more JS testing

--- samples/js/js.js
+++ samples/js/js.js
@@ -1,14 +1,11 @@
 function() {
-  var myString = attr("1. String only");
+  var nothing = attr("0. String only");
+  var myString = tr("1. String only");
   var anotherString = tr("2. String with description", "This is the description");
   var singleQuotesMyString = tr('3. String only');
   var singleQuoteAnotherString = tr("4. String with description", "This is the description");
   var withToken = tr("5. String with {token}", {"token": "my token"});
-  var withTMLToken = tr("6. String with {token}", "a description", {"token": tr("my localized token")});
-  
-  // var myString = tr("String only");
-  // var anotherString = tr("String with description", "This is the description");
-  // var withTMLToken = tr("String with {token}", {"token": tr("my token")});
-  // var tert = tr((really ? "One of two" : "Second of two"), "Description");
-  // var tert = tr((really ? "One of two" : tr("Second of two")), "Description");
+  var withTMLToken = tr("6. String with {token}", "a description", {"token": tr("7. my localized token")});
+  var tert = tr((really ? "8.1. One of two" : "8.2. Second of two"), "Description");
+  var tert = tr((really ? "9.1. One of two" : tr("9.2. Second of two")), "Description");
 }
