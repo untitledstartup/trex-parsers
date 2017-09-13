ea69aeade300c26e444876752da033c361e39b97
Addison Brickey <addbrick@gmail.com>
2017-09-13 15:17:52 -0700

    update runthis.js to put the output into a file instead of in the console and add that file to gitignore

--- runthis.js
+++ runthis.js
@@ -7,23 +7,32 @@ var genstringsMacro = "NSLocalizedString";
 var genstringsLanguage = "swift";
 var folderPath = '../../TestProjects/FirstTranslation/FirstTranslation'; // The relative folder path which contains the files you want to parse
 var filePathFilter = ['**/*.swift']; // File path filters, is passed into `fs.recurseSync` see https://www.npmjs.com/package/file-system#recursesyncdirpath-filter-callback
+var outputFilePath = './parse_results.json';
 
 
 // Script Variables. Do NOT Touch!
 var genstrings = new Genstrings();
 genstrings.macro = genstringsMacro;
 genstrings.language = genstringsLanguage;
+var translationKeysArray = []; // Where all the translation keys from progress are kept, so we can output at the end
 
 
 // Warning and Progess Callbacks
 genstrings.on("warning", function(message, info) {
   console.log('warning :: ' + message + ' ::');
   console.log(info);
+  console.log('');
 });
 genstrings.on("progress", function(progress, file, translationKeys) {
-  console.log('progress :: ' + progress + ' ::');
-  console.log(file);
-  console.log(translationKeys);
+  console.log('progress :: ' + progress + ' :: ' + file + ' ::');
+  translationKeysArray = translationKeysArray.concat(translationKeys); // Store for output at the end
+
+  // If we have finished parsing the files, output all the translation keys
+  if (progress >= 1)  {
+    fs.writeFile(outputFilePath, JSON.stringify(translationKeysArray), function() {
+      console.log('Script complete!! Output written to ' + outputFilePath);
+    });
+  }
 });
 
 
